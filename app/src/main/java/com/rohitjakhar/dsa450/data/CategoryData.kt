43124c4a1eded562cc86d2f910ce8c7a0f802bfe
impalakdwivedi <aapalakdwivedi@gmail.com>
2021-05-03 17:07:09 +0530

    Add Category Data issue has been resolved. ArrayQuestion also completed.

--- app/src/main/java/com/rohitjakhar/dsa450/data/CategoryData.kt
+++ app/src/main/java/com/rohitjakhar/dsa450/data/CategoryData.kt
@@ -12,7 +12,72 @@ object CategoryData {
     val StringQuestion = Category(
         categoryId = 2,
         categoryName = "String",
-        categoryTotalQuestion = 40
+        categoryTotalQuestion = 40,
+    )
+    val MatrixQuestion = Category(
+        categoryId = 3,
+        categoryName = "Matrix",
+        categoryTotalQuestion = 10,
+    )
+    val SearchingQuestion = Category(
+        categoryId = 4,
+        categoryName = "Searching",
+        categoryTotalQuestion = 36,
+    )
+    val LinkedlistQuestion = Category(
+        categoryId = 5,
+        categoryName = "Linkedlist",
+        categoryTotalQuestion = 36,
+    )
+    val BinaryTreesQuestion = Category(
+        categoryId = 6,
+        categoryName = "BinaryTrees",
+        categoryTotalQuestion = 35,
+    )
+    val BSTQuestion = Category(
+        categoryId = 7,
+        categoryName = "BST",
+        categoryTotalQuestion = 10,
+    )
+    val GreedyQuestion = Category(
+        categoryId = 8,
+        categoryName = "Greedy",
+        categoryTotalQuestion = 10,
+    )
+    val BacktrackingQuestion = Category(
+        categoryId = 9,
+        categoryName = "Backtracking",
+        categoryTotalQuestion = 35,
+    )
 
+    val StackQueuesQuestion = Category(
+        categoryId = 10,
+        categoryName = "StackQueues",
+        categoryTotalQuestion = 38,
+    )
+    val HeapQuestion = Category(
+        categoryId = 11,
+        categoryName = "Heap",
+        categoryTotalQuestion = 18,
+    )
+    val GraphQuestion = Category(
+        categoryId = 12,
+        categoryName = "Graph",
+        categoryTotalQuestion = 44,
+    )
+    val TrieQuestion = Category(
+        categoryId = 13,
+        categoryName = "Trie",
+        categoryTotalQuestion = 6,
+    )
+    val DynamicPQuestion = Category(
+        categoryId = 14,
+        categoryName = "DynamicP",
+        categoryTotalQuestion = 60,
+    )
+    val BitManipulationQuestion = Category(
+        categoryId = 15,
+        categoryName = "BitManipulation",
+        categoryTotalQuestion = 10,
     )
 }
--- app/src/main/java/com/rohitjakhar/dsa450/data/questiondata/ArrayQuestion.kt
+++ app/src/main/java/com/rohitjakhar/dsa450/data/questiondata/ArrayQuestion.kt
@@ -6,7 +6,220 @@ object ArrayQuestion {
     val arr1 = Question(
         questionIndex = 1,
         questionTopic = "Array",
-        questionStatement = "",
-        questionLink = ""
+        questionStatement = "Reverse an array or String",
+        questionLink = "https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/",
+    )
+
+    val arr2 = Question(
+        questionIndex = 2,
+        questionTopic = "Array",
+        questionStatement = "Find the maximum and minimum elements in an array",
+        questionLink = "https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/",
+    )
+    val arr3 = Question(
+        questionIndex = 3,
+        questionTopic = "Array",
+        questionStatement = "Find the Kth max and min element of an array",
+        questionLink = "https://practice.geeksforgeeks.org/problems/kth-smallest-element/0",
+    )
+    val arr4 = Question(
+        questionIndex = 4,
+        questionTopic = "Array",
+        questionStatement = "Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order",
+        questionLink = "https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1",
+    )
+
+    val arr5 = Question(
+        questionIndex = 5,
+        questionTopic = "Array",
+        questionStatement = "Move all negative numbers to beginning and positive to end with constant extra space",
+        questionLink = "https://www.geeksforgeeks.org/move-negative-numbers-beginning-positive-end-constant-extra-space/",
+    )
+    val arr6 = Question(
+        questionIndex = 6,
+        questionTopic = "Array",
+        questionStatement = "Find the Union and Intersection of the two sorted arrays",
+        questionLink = "https://practice.geeksforgeeks.org/problems/union-of-two-arrays/0",
+    )
+
+    val arr7 = Question(
+        questionIndex = 7,
+        questionTopic = "Array",
+        questionStatement = "Rotate the array by one position in clock-wise direction",
+        questionLink = "https://practice.geeksforgeeks.org/problems/cyclically-rotate-an-array-by-one2614/1",
+    )
+    val arr8 = Question(
+        questionIndex = 8,
+        questionTopic = "Array",
+        questionStatement = "Find the contiguous sub-array with maximum sum.",
+        questionLink = "https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1",
+    )
+    val arr9 = Question(
+        questionIndex = 9,
+        questionTopic = "Array",
+        questionStatement = "Minimise the maximum difference between heights [V.IMP]",
+        questionLink = "https://practice.geeksforgeeks.org/problems/minimize-the-heights3351/1",
+    )
+    val arr10 = Question(
+        questionIndex = 10,
+        questionTopic = "Array",
+        questionStatement = "Minimum number of jumps",
+        questionLink = "https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1,"
+    )
+    val arr11 = Question(
+        questionIndex = 11,
+        questionTopic = "Array",
+        questionStatement = "Find Duplicate in an array of N+1 Integers",
+        questionLink = "https://leetcode.com/problems/find-the-duplicate-number/",
+    )
+    val arr12 = Question(
+        questionIndex = 12,
+        questionTopic = "Array",
+        questionStatement = "Merge 2 sorted arrays without using Extra space.",
+        questionLink = "https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays5135/1",
+    )
+    val arr13 = Question(
+        questionIndex = 13,
+        questionTopic = "Array",
+        questionStatement = "Kadane's Algo [V.V.V.V.V IMP]",
+        questionLink = "https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1",
+    )
+    val arr14 = Question(
+        questionIndex = 14,
+        questionTopic = "Array",
+        questionStatement = "Merge Intervals",
+        questionLink = "https://leetcode.com/problems/merge-intervals/",
+    )
+    val arr15 = Question(
+        questionIndex = 15,
+        questionTopic = "Array",
+        questionStatement = "Next Permutation",
+        questionLink = "https://leetcode.com/problems/next-permutation/",
+    )
+    val arr16 = Question(
+        questionIndex = 16,
+        questionTopic = "Array",
+        questionStatement = "Count Inversion",
+        questionLink = "https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1",
+    )
+    val arr17 = Question(
+        questionIndex = 17,
+        questionTopic = "Array",
+        questionStatement = "Best time to buy and Sell stock",
+        questionLink = "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/",
+    )
+    val arr18 = Question(
+        questionIndex = 18,
+        questionTopic = "Array",
+        questionStatement = "Find all pairs on integer array whose sum is equal to given number",
+        questionLink = "https://practice.geeksforgeeks.org/problems/count-pairs-with-given-sum5022/1",
+    )
+    val arr19 = Question(
+        questionIndex = 19,
+        questionTopic = "Array",
+        questionStatement = "Find common elements In 3 sorted arrays",
+        questionLink = "https://practice.geeksforgeeks.org/problems/common-elements1132/1",
+    )
+    val arr20 = Question(
+        questionIndex = 20,
+        questionTopic = "Array",
+        questionStatement = "Rearrange the array in alternating positive and negative items with O(1) extra space",
+        questionLink = "https://www.geeksforgeeks.org/rearrange-array-alternating-positive-negative-items-o1-extra-space/",
+    )
+    val arr21 = Question(
+        questionIndex = 21,
+        questionTopic = "Array",
+        questionStatement = "Find if there is any subarray with sum equal to 0",
+        questionLink = "https://practice.geeksforgeeks.org/problems/subarray-with-0-sum-1587115621/1",
+    )
+    val arr22 = Question(
+        questionIndex = 22,
+        questionTopic = "Array",
+        questionStatement = "Find factorial of a large number",
+        questionLink = "https://practice.geeksforgeeks.org/problems/factorials-of-large-numbers2508/1",
+    )
+    val arr23 = Question(
+        questionIndex = 23,
+        questionTopic = "Array",
+        questionStatement = "Find maximum product subarray",
+        questionLink = "https://practice.geeksforgeeks.org/problems/maximum-product-subarray3604/1",
+    )
+    val arr24 = Question(
+        questionIndex = 24,
+        questionTopic = "Array",
+        questionStatement = "Find longest consecutive subsequence",
+        questionLink = "https://practice.geeksforgeeks.org/problems/longest-consecutive-subsequence2449/1",
+    )
+    val arr25 = Question(
+        questionIndex = 25,
+        questionTopic = "Array",
+        questionStatement = "Given an array of size n and a number k, find all elements that appear more than \" n/k \" times",
+        questionLink = "https://leetcode.com/problems/majority-element-ii/",
+    )
+    val arr26 = Question(
+        questionIndex = 26,
+        questionTopic = "Array",
+        questionStatement = "Maximum profit by buying and selling a share atmost twice",
+        questionLink = "https://www.geeksforgeeks.org/maximum-profit-by-buying-and-selling-a-share-at-most-twice/",
+    )
+    val arr27 = Question(
+        questionIndex = 27,
+        questionTopic = "Array",
+        questionStatement = "Find whether an array is a subset of another array",
+        questionLink = "https://practice.geeksforgeeks.org/problems/array-subset-of-another-array2317/1",
+    )
+    val arr28 = Question(
+        questionIndex = 28,
+        questionTopic = "Array",
+        questionStatement = "Find the triplet that sum to a given value",
+        questionLink = "https://practice.geeksforgeeks.org/problems/triplet-sum-in-array-1587115621/1",
+    )
+    val arr29 = Question(
+        questionIndex = 29,
+        questionTopic = "Array",
+        questionStatement = "Trapping Rain water problem",
+        questionLink = "https://practice.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1",
+    )
+    val arr30 = Question(
+        questionIndex = 30,
+        questionTopic = "Array",
+        questionStatement = "Chocolate Distribution problem",
+        questionLink = "https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1",
+    )
+    val arr31 = Question(
+        questionIndex = 31,
+        questionTopic = "Array",
+        questionStatement = "Smallest Sub Array with sum greater than a given value",
+        questionLink = "https://practice.geeksforgeeks.org/problems/smallest-subarray-with-sum-greater-than-x5651/1",
+    )
+    val arr32 = Question(
+        questionIndex = 32,
+        questionTopic = "Array",
+        questionStatement = "Three way partitioning of an array around a given value",
+        questionLink = "https://practice.geeksforgeeks.org/problems/three-way-partitioning/1",
+    )
+    val arr33 = Question(
+        questionIndex = 33,
+        questionTopic = "Array",
+        questionStatement = "Minimum swaps required bring elements less equal K together",
+        questionLink = "https://practice.geeksforgeeks.org/problems/minimum-swaps-required-to-bring-all-elements-less-than-or-equal-to-k-together/0",
+    )
+    val arr34 = Question(
+        questionIndex = 34,
+        questionTopic = "Array",
+        questionStatement = "Minimum no. of operations required to make an array palindrome",
+        questionLink = "https://practice.geeksforgeeks.org/problems/palindromic-array/0",
+    )
+    val arr35 = Question(
+        questionIndex = 35,
+        questionTopic = "Array",
+        questionStatement = "Median of 2 sorted arrays of equal size",
+        questionLink = "https://practice.geeksforgeeks.org/problems/find-the-median0527/1",
+    )
+    val arr36 = Question(
+        questionIndex = 36,
+        questionTopic = "Array",
+        questionStatement = "Median of 2 sorted arrays of different size",
+        questionLink = "https://www.geeksforgeeks.org/median-of-two-sorted-arrays-of-different-sizes/",
     )
 }
