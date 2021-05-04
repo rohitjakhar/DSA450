bbc753dcb220a36bd37283c1a7080c456281eee9
Rohit Jakhar <rohitjakhar940@gmail.com>
2021-05-04 20:41:29 +0530

    implement browser for open link of question

--- app/src/main/java/com/rohitjakhar/dsa450/ui/questionlist/QuestionList.kt
+++ app/src/main/java/com/rohitjakhar/dsa450/ui/questionlist/QuestionList.kt
@@ -1,6 +1,9 @@
 package com.rohitjakhar.dsa450.ui.questionlist
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,15 +16,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 
 @Composable
 fun QuestionList(
     id: Int?,
-    questionListViewModel: QuestionListViewModel,
-    navController: NavController
+    questionListViewModel: QuestionListViewModel
 ) {
+    val context = LocalContext.current
     val questionList = id?.let { questionListViewModel.getQuestionList(it) }
     LazyColumn(
         content = {
@@ -31,6 +34,11 @@ fun QuestionList(
                     Card(
                         modifier = Modifier
                             .padding(8.dp)
+                            .clickable {
+                                val intent = Intent(Intent.ACTION_VIEW)
+                                intent.data = Uri.parse(question.questionLink)
+                                context.startActivity(intent)
+                            }
                             .border(1.dp, Color.Cyan, RoundedCornerShape(CornerSize(8.dp)))
                     ) {
                         Column(
