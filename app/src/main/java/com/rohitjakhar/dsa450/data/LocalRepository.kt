6b34b3f7e47a47bbc552aa8e604c17c6ada91ea4
Rohit Jakhar <rohitjakhar940@gmail.com>
2021-05-04 11:28:07 +0530

    update Category data

--- app/src/main/java/com/rohitjakhar/dsa450/data/LocalRepository.kt
+++ app/src/main/java/com/rohitjakhar/dsa450/data/LocalRepository.kt
@@ -3,6 +3,19 @@ package com.rohitjakhar.dsa450.data
 class LocalRepository {
     val listOfCategory = listOf(
         CategoryData.ArrayQuestion,
-        CategoryData.StringQuestion
+        CategoryData.StringQuestion,
+        CategoryData.MatrixQuestion,
+        CategoryData.SearchingQuestion,
+        CategoryData.LinkedlistQuestion,
+        CategoryData.BinaryTreesQuestion,
+        CategoryData.BSTQuestion,
+        CategoryData.GreedyQuestion,
+        CategoryData.BacktrackingQuestion,
+        CategoryData.StackQueuesQuestion,
+        CategoryData.HeapQuestion,
+        CategoryData.GraphQuestion,
+        CategoryData.TrieQuestion,
+        CategoryData.DynamicPQuestion,
+        CategoryData.BitManipulationQuestion,
     )
 }
